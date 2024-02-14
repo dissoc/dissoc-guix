0ead76c238f0902e96baacdd62aaf0b6e7275849
Justin Bishop <mail@dissoc.me>
2024-02-14 00:19:55 -0600

    added: python-pypubsub and renamed timeago-python to python-timeago

--- dissoc/gnu/packages/python-xyz.scm
+++ dissoc/gnu/packages/python-xyz.scm
@@ -8,9 +8,9 @@
   #:use-module (guix git-download)
   #:use-module (guix packages))
 
-(define-public timeago-python
+(define-public python-timeago
   (package
-   (name "timeago-python")
+   (name "python-timeago")
    (version "1.0.16")
    (source
     (origin
@@ -28,3 +28,24 @@
     "A very simple python lib, used to format datetime with *** time ago
 statement.")
    (license license:expat)))
+
+(define-public python-pypubsub
+  (package
+   (name "python-pypubsub")
+   (version "4.0.3")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/schollii/pypubsub.git")
+           (commit (string-append "v" version))))
+     (sha256
+      (base32 "02j74w28wzmdvxkk8i561ywjgizjifq3hgcl080yj0rvkd3wivlb"))))
+   (build-system python-build-system)
+   (arguments `(#:tests? #f)) ; fails to import test modules
+   (home-page "https://github.com/schollii/pypubsub")
+   (synopsis "A Python publish-subcribe library")
+   (description
+    "Provides a publish-subscribe API to facilitate event-based or message-based
+architecture in a single-process application.")
+   (license license:bsd-2)))
